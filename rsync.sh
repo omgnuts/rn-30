82eef0878b65e6d0afcc74c382094c2013153aaa
thecookiejar <nil>
2016-09-04 00:01:47 +0800

    update rsync

--- rsync.sh
+++ rsync.sh
@@ -2,12 +2,14 @@
 
 SOURCE=~/react-native/30-days-of-react-native/
 
+#--include="*.js" \
 
-echo $SOURCE
-
-rsync -a --no-perms --no-owner --no-group \
+rsync -avrn --no-perms --no-owner --no-group \
 --include="src/" \
 --include="index.android.js" \
 --include="index.ios.js" \
---exclude="*" \
+--exclude="*/" \
+--exclude=".*" \
+--exclude="*.json" \
+--exclude="*.sh" \
 $SOURCE . 
